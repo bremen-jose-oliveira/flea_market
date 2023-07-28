4509a77f06822d670fe046b220375b915801b76e
Jose Oliveira <108938041+bremen-jose-oliveira@users.noreply.github.com>
2023-07-28 15:37:24 +0200

    added email validation

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import * as Yup from 'yup';
 
 
 
@@ -6,8 +7,19 @@ import User from "../models/User";
 class SessionController {
 
    async store(req, res){
+
+     const schema = Yup.object().shape({
+          email: Yup.string().email().required(),
+     });
+
+
         
         const {email} = req.body;
+
+        if(!(await schema.isValid(req.body))){
+
+          return res.status(400).json({ error: " bad request"});
+        }
         
         let user = await User.findOne({email});
         
